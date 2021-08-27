eb8bb912b716a10faba40843fe7328d89d3e16c2
Torey Littlefield <52614742+toreylittlefield@users.noreply.github.com>
2021-08-27 09:29:58 -0400

    clean up

--- src/Content_Scripts/netflix_video.js
+++ src/Content_Scripts/netflix_video.js
@@ -4,10 +4,8 @@ const attachToVideo = () => {
   const videoElement = () => document.querySelector('video');
   const intervalTimer = setInterval(() => {
     if (videoElement()) {
-      console.count('video found');
       attachVideoListener(window.document.body, true);
       console.log('adding listeners for netflix...');
-      console.log(videoElement());
       clearInterval(intervalTimer);
     }
   }, 1000);
